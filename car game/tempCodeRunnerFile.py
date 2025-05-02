63ec48a0a65f98b13cf1ffe65e226ce90e6ce67a
chirag1683 <chiraggupta89558@gmail.com>
2025-05-02 21:53:45 +0530

    Add files via upload

---
+++ car game/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ car3 = pygame.image.load("C:/Users/chirag/OneDrive/Desktop/python/python-beginner-projects/projects/car game/car 3.png")
+    car3X = random.randint(178, 490)
+    car3Y = 100
+    car3Ychange = 10
